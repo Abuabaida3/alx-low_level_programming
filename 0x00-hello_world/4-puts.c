8d039ccf78eb5790438ddd27dd2569d130c396e8
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-10-05 13:06:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - prints the phras "programming is like buliding a multilingual puzzle
- * Ruturn:0 if exited properly,non-zero otherwise
+ * main - prints a text
+ *
+ * Returnt:Always 0 (Success)
  */
 int main(void)
-
- {
-
-puts("\"programming is like building a multilingual puzzle");
-
- return (0);
-
-
- }
+{
+puts("\"programming is like a building multilingual puzzle");
+return (0);
+}
