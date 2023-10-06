c0b60309b39c68f90625d1d32ab88d4491d540d0
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-10-06 11:43:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * int main - prints the alphabet in lowercase
+ * followed by a new line
+ * Return: Always 0 (Sccess)
+ * int main (void)
+ * {
+ * char ch;
+ *
+ * for (ch = 'a'; ch = 'a'; ch <= 'z'; ch++)
+ * {
+ * putchar(ch);
+ * }
+ * putchar('\n')
+ * return (0);
+ * }
