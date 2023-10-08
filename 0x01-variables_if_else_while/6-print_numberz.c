15e2f5c2c02ea15e7afbd12d9b153390c2f80070
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-10-08 12:01:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * int main - prints all singie digit numbers  bas 10 starting from 0;
+ * followed by new line using putchar
+ * Rturn: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(0);
+	}
+	putchar('\n');
+		return (0);
+}
