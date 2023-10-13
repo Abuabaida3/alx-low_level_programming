3731c98db2cbf69795901e0255a614b979322440
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-10-13 09:55:49 -0700

    1-main.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more numbers - cheks for cheks for a digit (0 through 9).
+ * Return: Always 0.
+ */
+void more_numbers 
+{
+	int a;  b'
+
+	for (b = 0; a< 10; a++)
+{
+	for (b = 0; b < 15; b++)
+	{
+		if (b/10 != 0)
+	{
+	_putchar(b / 10 + '10');
+	}
+             _putchar(b % 10 + '0');
+	}
+	_putchar('\n');
+}
+}
+                                                   }
