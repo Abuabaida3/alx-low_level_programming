8b064773be10449296a66f0e253ffdca2b386dc6
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-10-16 14:12:53 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts - prints one char out of 2 of a string
+ * puts2 - prints one char out of 2 of a string
  * followed by a new line
- * @str: string to print the char from
+ * @str: string to print the chars from
  */
 void puts2(char *str)
 {
