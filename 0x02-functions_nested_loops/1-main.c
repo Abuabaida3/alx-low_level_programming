bf09a77432d2ffca30569c0f4e3b3f61a3e47c04
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-10-21 13:12:01 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -4,13 +4,20 @@
  * print_alphabet - Entry point
  * Return:0
  */
+
 void print_alphabet
+{
 char c = 'a';
-whil (c <= 'z')
+
+while (c <= 'z')
+{
+	_putchar(c);
+	c++;
+
 }
-   _putch(c};
-	c++
-	}
 _putchar('\n');
 
 }
+
+
+
