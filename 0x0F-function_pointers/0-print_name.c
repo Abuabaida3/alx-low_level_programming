747cab7a74d0c7394fef6f7750922c668d14d2e5
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-11-08 09:54:35 -0800

    my first

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
 * print_name - prints a name
 * @name: input string
-* @f: function casted to char pointer
+* @f: function pointer casted to char pointer
 */
 void print_name(char *name, void (*f)(char *))
 {
