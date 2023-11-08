f19b49440ef563c29614f842cfacb5031ca6cc36
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-11-08 11:16:47 -0800

    my first

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
 /**
-* print_name - prints a name
-* @name: input string
-* @f: function pointer casted to char pointer
+* print_name - prints a name.
+* @name: name to print.
+* @f: function to print name.
+*
 */
 void print_name(char *name, void (*f)(char *))
 {
-(*f)(name);
+if (name && f)
+	f(name);
 }
 
