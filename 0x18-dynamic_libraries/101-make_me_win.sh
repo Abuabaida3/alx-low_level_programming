21398ce8b5db90fc615a198439a2c95698cf3906
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-19 12:17:03 -0800

    my first

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p ..https://raw.githubusercontent.com/tolulope05/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+wget -p ..https://raw.githubusercontent.com/Abuabaida3/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
