728faedf53d65242002bf0f1585a46062994b300
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-19 12:46:38 -0800

    myfirst

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -20,9 +20,9 @@ int div(int a, int b)
 	{
 		return a / b;
 	}
-	int mod(int a, int b)
-		if (b == 0)
-	{
+	int mod(int a, int b){
+		if (b == 0){
+	
 			printf("Error: division by zero\n");
 				return 0;
 			
